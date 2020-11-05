0680cdd5d56c4684ce361959db2c89cf56f35624
namantia1@gmail.com <namantia1@gmail.com>
2020-11-05 17:04:12 -0500

    update

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/leaf.png', //Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+ // This path is subpath of your hosting https://domain/portfolio
   heading: 'Nick Amantia',
   subHeading: 'Web Developer.',
   // social
